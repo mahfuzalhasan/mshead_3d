5876421d503bd4770ef9595caf7cdc24dd5e3164
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-03-19 16:36:49 -0400

    checking Segment18_Color keys

--- data_process_lymph_node.py
+++ data_process_lymph_node.py
@@ -21,6 +21,8 @@ patient_ids = os.listdir(data_dir)
 # for pat_id in T(patient_ids):
 # def data_processing(args):
     # pat_id, num_slices = args
+presence = []
+absence = []
 for pat_id in T(patient_ids):
     pat_id = str(pat_id)
     print(f'############## patient id ###############:{pat_id}')
@@ -28,7 +30,7 @@ for pat_id in T(patient_ids):
         data_file = [f for f in os.listdir(os.path.join(data_dir, pat_id))if 'IM00' in f][0]
     except:
         print(f"Problem with {pat_id}")    
-    seg_file = [f for f in os.listdir(os.path.join(data_dir, pat_id))if 'Segmentation.seg' in f][0]
+    seg_file = [f for f in os.listdir(os.path.join(data_dir, pat_id))if 'Segmentation_v2' in f][0]
     # seg2_file = seg_file.replace('Segmentation', 'Segmentation_v2')
 
     img_pat_id, img_header = nrrd.read(os.path.join(data_dir, pat_id, data_file))
@@ -37,6 +39,7 @@ for pat_id in T(patient_ids):
     # print('img header: ', img_header.keys())
     if 'Segment18_Color' in img_header.keys():
         print("True in img_header")
+        
     else:
         print("False in img_header")
 
@@ -45,8 +48,10 @@ for pat_id in T(patient_ids):
     # print('mask header: ', mask_header.keys())
     if 'Segment18_Color' in mask_header.keys():
         print("True in mask_header")
+        presence.append(pat_id)
     else:
         print("False in mask_header")
+        absence.append(pat_id)
 
     continue
     # print('mask info: ',mask_pat_id.shape, np.min(mask_pat_id), np.max(mask_pat_id))
@@ -59,5 +64,7 @@ for pat_id in T(patient_ids):
     # saving transposed data and mask in new directory
     nrrd.write(os.path.join(new_patient_path, data_file), data_switched, header=img_header)
     nrrd.write(os.path.join(new_patient_path, seg_file), mask_switched, header=mask_header)
+print('presence: ', presence)
+print('absence: ', absence)
     
 
