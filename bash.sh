bc9184dc6d96581244421c311d590295a6e5e5e5
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-03-20 14:43:56 -0400

    updated bash and learning rate

--- bash.sh
+++ bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=python_train_LN_3D
-#SBATCH --output=/blue/r.forghani/share/scripts/mshead_3d/results/job.%J.out
-#SBATCH --error=/blue/r.forghani/share/scripts/mshead_3d/results/job.%J.err
+#SBATCH --output=/blue/r.forghani/scripts/mshead_3d/results/job.%J.out
+#SBATCH --error=/blue/r.forghani/scripts/mshead_3d/results/job.%J.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=m.tahsinmostafiz@ufl.edu
 #SBATCH --nodes=1
@@ -17,8 +17,8 @@
 module load conda
 conda activate medical
 
-cd /blue/r.forghani/share/scripts/mshead_3d
+cd /blue/r.forghani/scripts/mshead_3d
 
 # Execute the Python script
-srun python main_train.py --dataset LN --network SNET --cache_rate 1.0 --batch_size 3 --crop_sample 4 --lr 0.00015 --num_workers $SLURM_CPUS_PER_TASK
+srun python main_train.py --dataset LN --network SNET --cache_rate 1.0 --batch_size 3 --crop_sample 4 --lr 0.0001 --num_workers $SLURM_CPUS_PER_TASK
 # srun python test_seg.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK
