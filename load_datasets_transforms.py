b50cbaa486fba715dcc7759955c810d2768d56a7
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-06-25 00:46:51 -0600

    fold print

--- load_datasets_transforms.py
+++ load_datasets_transforms.py
@@ -50,6 +50,7 @@ def data_loader(args):
         train_samples = {}
         valid_samples = {}
         print(f'#### loading training and validation set ########## \n')
+        print(f'Training on fold:{args.fold}')
         
         val_per_fold = 69
         start_index = val_per_fold * args.fold
