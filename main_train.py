cbc528a91aae6ec83375e93244e7d350751fa328
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-07-09 21:55:05 -0600

    resuming fold 4

--- main_train.py
+++ main_train.py
@@ -47,7 +47,7 @@ parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate for 
 parser.add_argument('--optim', type=str, default='AdamW', help='Optimizer types: Adam / AdamW')
 parser.add_argument('--max_iter', type=int, default=40000, help='Maximum iteration steps for training')
 parser.add_argument('--eval_step', type=int, default=500, help='Per steps to perform validation')
-parser.add_argument('--resume', default=False, help='resume training from an earlier iteration')
+parser.add_argument('--resume', default=True, help='resume training from an earlier iteration')
 ## Efficiency hyperparameters
 parser.add_argument('--gpu', type=int, default=0, help='your GPU number')
 parser.add_argument('--cache_rate', type=float, default=1, help='Cache rate to cache your dataset into memory')
@@ -279,7 +279,7 @@ print(f'$$$$$$$$$$$$$ run_id:{run_id} $$$$$$$$$$$$$')
 ### run with python main_train.py --resume True
 ### Then set model_path here
 if args.resume:
-    model_path = '/orange/r.forghani/results/02-25-24_0431/model_15750.pth'
+    model_path = '/orange/r.forghani/results/06-26-24_2259/model_36500.pth'
     state_dict = torch.load(model_path)
     model.load_state_dict(state_dict['model'])
     optimizer.load_state_dict(state_dict['optimizer'])
