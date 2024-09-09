af07751a71f0b6f86957c59be7b2d30ca5ca354e
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-09-09 03:05:14 -0400

    debugging

--- networks/msHead_3D/network_backbone.py
+++ networks/msHead_3D/network_backbone.py
@@ -54,7 +54,7 @@ class MSHEAD_ATTN(nn.Module):
         in_chans=1,
         out_chans=13,
         depths=[2, 2, 2, 2],
-        feat_size=[48, 96, 192, 384],
+        feat_size=[48, 96, 192, 384, 768],
         num_heads = [3, 6, 12, 24],
         drop_path_rate=0,
         layer_scale_init_value=1e-6,
