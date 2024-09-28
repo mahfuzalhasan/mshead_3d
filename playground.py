0bf0da4f6bfb6c74951944218d96af31d5671e76
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-09-28 01:54:54 -0400

    debugging other models

--- playground.py
+++ playground.py
@@ -44,12 +44,61 @@ import torch.nn.functional as F
 
 
 # # Example input: a batch of 1-channel images, size 64x64
-import ptwt
-import torch
-data = torch.randn(6,16,48,48,48)
-for i in range(1, 4):
-    coeffs = ptwt.wavedec3(data, wavelet="haar", level=i)
-    Y1 = coeffs[0]
-    print(Y1.shape)
-# print([(key, coeff.shape) for key, coeff in ptwt.wavedec3(data, wavelet="haar", level=2)[-1].items()])
+# import ptwt
+# import torch
+# data = torch.randn(6,16,48,48,48)
+# for i in range(1, 4):
+#     coeffs = ptwt.wavedec3(data, wavelet="haar", level=i)
+#     Y1 = coeffs[0]
+#     print(Y1.shape)
+# # print([(key, coeff.shape) for key, coeff in ptwt.wavedec3(data, wavelet="haar", level=2)[-1].items()])
+from networks.UXNet_3D.network_backbone import UXNET
+from networks.msHead_3D.network_backbone import MSHEAD_ATTN
+from monai.networks.nets import UNETR, SwinUNETR
 
+
+out_classes = 4
+
+device = torch.device("cuda")
+print(f'--- device:{device} ---')
+model_1 = UNETR(
+        in_channels=1,
+        out_channels=out_classes,
+        img_size=(96, 96, 96),
+        feature_size=16,
+        hidden_size=768,
+        mlp_dim=3072,
+        num_heads=12,
+        pos_embed="perceptron",
+        norm_name="instance",
+        res_block=True,
+        dropout_rate=0.0,
+    ).to(device)
+
+data = torch.randn(2, 1, 96, 96, 96)  # Example tensor with B=1, C=3, D=64, H=64, W=64
+y = model_1(data)
+print(f'y from UNETR: {y.shape}')
+
+model_2 = SwinUNETR(
+        img_size=(96, 96, 96),
+        in_channels=1,
+        out_channels=out_classes,
+        feature_size=48,
+        use_checkpoint=False,
+    ).to(device)
+
+y = model_2(data)
+print(f'y from SwinUNETR: {y.shape}')
+
+model_3 = UXNET(
+        in_chans=1,
+        out_chans=out_classes,
+        depths=[2, 2, 2, 2],
+        feat_size=[48, 96, 192, 384],
+        drop_path_rate=0,
+        layer_scale_init_value=1e-6,
+        spatial_dims=3,
+    ).to(device)
+
+y = model_3(data)
+print(f'y from 3D UXNET: {y.shape}')
