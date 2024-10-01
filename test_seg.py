7fbb0b25ca6cc8d1e2a3343b7dd06ddffaa20c1b
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-10-01 13:57:42 -0400

    testing ready

--- test_seg.py
+++ test_seg.py
@@ -75,8 +75,9 @@ if args.network == 'MSHEAD':
         in_chans=1,
         out_chans=out_classes,
         depths=[2,2,2,2],
-        feat_size=[48,96,192,384,768],
+        feat_size=[48,96,192,384],
         num_heads = [3,6,12,24],
+        local_region_scales = [2, 2, 1, 1],
         use_checkpoint=False,
     ).to(device)
 
@@ -91,19 +92,19 @@ elif args.network == 'SwinUNETR':
 
 if args.fold == 0:
     # args.trained_weights = '/orange/r.forghani/results/09-18-24_0219/model_best.pth'
-    args.trained_weights = '/orange/r.forghani/results/09-29-24_0356/model_best.pth'
+    args.trained_weights = '/orange/r.forghani/results/09-30-24_2152/model_best.pth'
 elif args.fold == 1:
     # args.trained_weights = '/orange/r.forghani/results/09-20-24_0448/model_best.pth'
-    args.trained_weights = '/orange/r.forghani/results/09-29-24_0413/model_best.pth'
+    args.trained_weights = '/orange/r.forghani/results/09-30-24_2200/model_best.pth'
 elif args.fold == 2:
     # args.trained_weights = '/orange/r.forghani/results/09-21-24_1416/model_best.pth'
-    args.trained_weights = '/orange/r.forghani/results/09-29-24_0424/model_best.pth'
+    args.trained_weights = '/orange/r.forghani/results/09-30-24_2213/model_best.pth'
 elif args.fold == 3:
     # args.trained_weights = '/orange/r.forghani/results/09-18-24_2221/model_best.pth'
-    args.trained_weights = '/orange/r.forghani/results/09-29-24_0441/model_best.pth'
+    args.trained_weights = '/orange/r.forghani/results/09-30-24_2239/model_best.pth'
 elif args.fold == 4:
     # args.trained_weights = '/orange/r.forghani/results/09-18-24_2224/model_best.pth'
-    args.trained_weights = '/orange/r.forghani/results/09-29-24_2046/model_best.pth'
+    args.trained_weights = '/orange/r.forghani/results/09-30-24_2258/model_best.pth'
 
 print(f'best model from fold:{args.fold} model path:{args.trained_weights}')
 state_dict = torch.load(args.trained_weights)
