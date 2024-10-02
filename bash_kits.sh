ad678ea0a2a73f81c0051e58e5e57e5ca89ce30f
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-10-01 20:05:37 -0400

    ready to train on kits

---
+++ bash_kits.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=python_train_Kits_ablation_wave-2-branch-v2
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wavelet-two-branch/mshead_3d/results/wave-2-branch_kits_job.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wavelet-two-branch/mshead_3d/results/wave-2-branch_kits_job.%J.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=128GB
+#SBATCH --partition=hpg-ai
+#SBATCH --gpus=a100:1
+#SBATCH --time=24:00:00
+
+module load conda
+conda activate waveformer
+
+cd /blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wavelet-two-branch/mshead_3d
+
+# Execute the Python script
+srun python main_train_kits.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --fold $FOLD
+# srun python test_seg.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --fold $FOLD
