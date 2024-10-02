ab755069da180ba442047e22676800f8043fd682
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-10-01 23:51:38 -0400

    paths and file names changed

--- bash_plot_amos.sh
+++ bash_plot_amos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=python_plot_flare_wavlet-wo-split-v2
-#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_flare/wavelet-wo-split-v2/mshead_3d/results/wavlet_wo_split_v2_plot_job.%J.out
-#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_flare/wavelet-wo-split-v2/mshead_3d/results/wavlet_wo_split_v2_plot_job.%J.err
+#SBATCH --job-name=python_plot_amos_wavlet-wo-split-v2
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_amos/wave-wo-split-v2/mshead_3d/results/wavlet_wo_split_v2_amos_plot_job.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_amos/wave-wo-split-v2/mshead_3d/results/wavlet_wo_split_v2_amos_plot_job.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
@@ -13,7 +13,7 @@
 module load conda
 conda activate waveformer
 
-cd /blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_flare/wavelet-wo-split-v2/mshead_3d
+cd /blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_amos/wave-wo-split-v2/mshead_3d
 
 # Execute the Python script
 srun python plotting_prediction_amos.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --pretrained_weights $PRETRAINED_WEIGHTS
