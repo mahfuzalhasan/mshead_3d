b15345a09821adf66e1c6ea4e571a68438927059
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-10-18 21:47:40 -0400

    bg=False for individual calculation

--- test_seg.py
+++ test_seg.py
@@ -154,7 +154,7 @@ with torch.no_grad():
         # --- Individual mean (across class) Dice score per sample ---
         for pred, label in zip(test_output_convert, test_labels_convert):
             # print(f'pred:{pred.shape} label:{label.shape}')
-            individual_dice_scores = DiceMetric(include_background=True, reduction="none")(pred.unsqueeze(0), label.unsqueeze(0))
+            individual_dice_scores = DiceMetric(include_background=False, reduction="none")(pred.unsqueeze(0), label.unsqueeze(0))
             individual_dice_score = torch.mean(individual_dice_scores).item()
             dice_vals_individual.append(individual_dice_score)
         
