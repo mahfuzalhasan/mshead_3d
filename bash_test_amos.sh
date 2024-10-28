0d470911e94f0b257801289659d47b13938a7d5b
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-10-28 17:38:19 -0400

    bash file params changed

--- bash_test_amos.sh
+++ bash_test_amos.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=python_ablation_wavelet-2-branch_amos_test
-#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_amos/wavelet-two-branch/mshead_3d/results/ablation_wavelet-2-branch_amos_test_job.%J.out
-#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_amos/wavelet-two-branch/mshead_3d/results/ablation_wavelet-2-branch_amos_test_job.%J.err
+#SBATCH --job-name=python_wf_2211_amos_test
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_amos/wavelet-two-branch/mshead_3d/results/wf_2211_amos_test_job.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_amos/wavelet-two-branch/mshead_3d/results/wf_2211_amos_test_job.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=128GB
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64GB
 #SBATCH --partition=hpg-ai
 #SBATCH --gpus=a100:1
-#SBATCH --time=24:00:00
+#SBATCH --time=1:00:00
 
 module load conda
 conda activate waveformer
