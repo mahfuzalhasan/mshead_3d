bb3b88d891d71581053a6fb0b939fa3e78316eaa
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-10-28 03:23:04 -0400

    update job params

--- bash.sh
+++ bash.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=python_train_kits_v2
-#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/project_analysis_kits19/mshead_3d/results/wavelet_max_ds_kits_v2_job.%J.out
-#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/project_analysis_kits19/mshead_3d/results/wavelet_max_ds_kits_v2_job.%J.err
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/project_analysis_kits19/mshead_3d/results/wf_1111_kits_v2_job.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/project_analysis_kits19/mshead_3d/results/wf_1111_kits_v2_job.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=128GB
 #SBATCH --partition=hpg-ai
 #SBATCH --gpus=a100:1
-#SBATCH --time=36:00:00
+#SBATCH --time=20:00:00
 
 module load conda
 conda activate waveformer
