a5209b95409820003a6d79e5a91bae6f795425e3
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-10-28 18:00:07 -0400

    multu-scale evaluation for amos

--- bash_ms_test.sh
+++ bash_ms_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=test_flare_wf_2211_fold_${FOLD}
-#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_flare/wavelet-two-branch/mshead_3d/results/fold_${FOLD}_wf_2211_job.%J.out
-#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_flare/wavelet-two-branch/mshead_3d/results/fold_${FOLD}_wf_2211_job.%J.err
+#SBATCH --job-name=test_amos_wf_2211
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_amos/wavelet-two-branch/mshead_3d/results/_wf_2211_amos_ms_test_job.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_amos/wavelet-two-branch/mshead_3d/results/wf_2211_amos_ms_test_job.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
@@ -13,8 +13,9 @@
 module load conda
 conda activate waveformer
 
-cd /blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_flare/wavelet-two-branch/mshead_3d
+cd /blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_amos/wavelet-two-branch/mshead_3d
 
 # Execute the Python script
 # srun python main_train.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --fold $FOLD
-srun python multi_scale_evaluation.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --fold $FOLD
+# srun python multi_scale_evaluation.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --fold $FOLD
+srun python multi_scale_evaluation.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --trained_weights $TRAINED_WEIGHTS
--- multi_scale_evaluation.py
+++ multi_scale_evaluation.py
@@ -33,10 +33,12 @@ import copy
 from utils import scale_wise_organ_filtration, filtering_output
 parser = argparse.ArgumentParser(description='3D UX-Net inference hyperparameters for medical image segmentation')
 ## Input data hyperparameters
-parser.add_argument('--root', type=str, default='/blue/r.forghani/share/flare_data', required=False, help='Root folder of all your images and labels')
+# parser.add_argument('--root', type=str, default='/blue/r.forghani/share/flare_data', required=False, help='Root folder of all your images and labels')
+# parser.add_argument('--output', type=str, default='/orange/r.forghani/results', required=False, help='Output folder for both tensorboard and the best model')
+# parser.add_argument('--dataset', type=str, default='flare', required=False, help='Datasets: {feta, flare, amos}, Fyi: You can add your dataset here')
+parser.add_argument('--root', type=str, default='/blue/r.forghani/share/amoss22/amos22', required=False, help='Root folder of all your images and labels')
 parser.add_argument('--output', type=str, default='/orange/r.forghani/results', required=False, help='Output folder for both tensorboard and the best model')
-parser.add_argument('--dataset', type=str, default='flare', required=False, help='Datasets: {feta, flare, amos}, Fyi: You can add your dataset here')
-
+parser.add_argument('--dataset', type=str, default='amos', required=False, help='Datasets: {feta, flare, amos}, Fyi: You can add your dataset here')
 ## Input model & training hyperparameters
 parser.add_argument('--network', type=str, default='MSHEAD', required=False, help='Network models: {TransBTS, nnFormer, UNETR, SwinUNETR, 3DUXNET}')
 parser.add_argument('--trained_weights', default='', required=False, help='Path of pretrained/fine-tuned weights')
@@ -114,22 +116,22 @@ elif args.network == 'SwinUNETR':
         feature_size=48,
         use_checkpoint=False,
     ).to(device)
-
-if args.fold == 0:
-    # args.trained_weights = '/orange/r.forghani/results/09-18-24_0219/model_best.pth'
-    args.trained_weights = '/orange/r.forghani/results/09-30-24_2152/model_best.pth'
-elif args.fold == 1:
-    # args.trained_weights = '/orange/r.forghani/results/09-20-24_0448/model_best.pth'
-    args.trained_weights = '/orange/r.forghani/results/09-30-24_2200/model_best.pth'
-elif args.fold == 2:
-    # args.trained_weights = '/orange/r.forghani/results/09-21-24_1416/model_best.pth'
-    args.trained_weights = '/orange/r.forghani/results/09-30-24_2213/model_best.pth'
-elif args.fold == 3:
-    # args.trained_weights = '/orange/r.forghani/results/09-18-24_2221/model_best.pth'
-    args.trained_weights = '/orange/r.forghani/results/09-30-24_2239/model_best.pth'
-elif args.fold == 4:
-    # args.trained_weights = '/orange/r.forghani/results/09-18-24_2224/model_best.pth'
-    args.trained_weights = '/orange/r.forghani/results/09-30-24_2258/model_best.pth'
+if args.dataset != 'amos':
+    if args.fold == 0:
+        # args.trained_weights = '/orange/r.forghani/results/09-18-24_0219/model_best.pth'
+        args.trained_weights = '/orange/r.forghani/results/09-30-24_2152/model_best.pth'
+    elif args.fold == 1:
+        # args.trained_weights = '/orange/r.forghani/results/09-20-24_0448/model_best.pth'
+        args.trained_weights = '/orange/r.forghani/results/09-30-24_2200/model_best.pth'
+    elif args.fold == 2:
+        # args.trained_weights = '/orange/r.forghani/results/09-21-24_1416/model_best.pth'
+        args.trained_weights = '/orange/r.forghani/results/09-30-24_2213/model_best.pth'
+    elif args.fold == 3:
+        # args.trained_weights = '/orange/r.forghani/results/09-18-24_2221/model_best.pth'
+        args.trained_weights = '/orange/r.forghani/results/09-30-24_2239/model_best.pth'
+    elif args.fold == 4:
+        # args.trained_weights = '/orange/r.forghani/results/09-18-24_2224/model_best.pth'
+        args.trained_weights = '/orange/r.forghani/results/09-30-24_2258/model_best.pth'
 
 print(f'best model from fold:{args.fold} model path:{args.trained_weights}')
 state_dict = torch.load(args.trained_weights)
