9cd90482e879fea9d9dbdfe04ff67c52fc2146a4
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-11-04 16:18:01 -0500

    bash for organ wise evaluation

--- test_job_files/bash_test_kits.sh
+++ test_job_files/bash_test_kits.sh
@@ -16,4 +16,4 @@ conda activate waveformer
 cd /blue/r.forghani/mdmahfuzalhasan/project_analysis_kits19/mshead_3d
 
 # Execute the Python script
-srun python test_seg.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --fold $FOLD --dataset $DATASET
+srun python organ_wise_evaluation.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --fold $FOLD --dataset $DATASET
