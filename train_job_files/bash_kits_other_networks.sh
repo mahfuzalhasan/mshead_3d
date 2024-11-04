be6327be6bc495567726df49690ab9cc8e1a5dc6
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-11-04 00:25:06 -0500

    bash file to train other networks

---
+++ train_job_files/bash_kits_other_networks.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=train_kits_other_network
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wavelet-two-branch/mshead_3d/results/kits_other_network_job.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wavelet-two-branch/mshead_3d/results/kits_other_network_job.%J.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=128GB
+#SBATCH --partition=hpg-ai
+#SBATCH --gpus=a100:1
+#SBATCH --time=24:00:00
+
+module load conda
+conda activate waveformer
+
+cd /blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wavelet-two-branch/mshead_3d
+
+# Execute the Python script
+srun python main_train.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --fold $FOLD --dataset $DATASET --network $NETWORK
