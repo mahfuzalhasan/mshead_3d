028be7eaffeb186a91608d04926556c19532b92f
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-11-06 01:35:03 -0500

    file name updated in bash

--- test_multiscale_job_files/bash_ms_test_kits.sh
+++ test_multiscale_job_files/bash_ms_test_kits.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=multiscale_kits_wf_3211
-#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wf_3211/mshead_3d/results/wf_3211_kits_ms_test_job.%J.out
-#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wf_3211/mshead_3d/results/wf_3211_kits_ms_test_job.%J.err
+#SBATCH --job-name=multiscale_kits_wf_4321
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wf_4321/mshead_3d/results/wf_4321_kits_ms_test_job.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wf_4321/mshead_3d/results/wf_4321_kits_ms_test_job.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
@@ -13,7 +13,7 @@
 module load conda
 conda activate waveformer
 
-cd /blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wf_3211/mshead_3d
+cd /blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wf_4321/mshead_3d
 
 # Execute the Python script
 # srun python main_train.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --fold $FOLD
--- train_job_files/bash_kits.sh
+++ train_job_files/bash_kits.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=train_kits_wf_3211
-#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wf_3211/mshead_3d/results/kits_train_3211_job.%J.out
-#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wf_3211/mshead_3d/results/kits_train_3211_job.%J.err
+#SBATCH --job-name=train_kits_wf_4321
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wf_4321/mshead_3d/results/kits_train_4321_job.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wf_4321/mshead_3d/results/kits_train_4321_job.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
@@ -13,7 +13,7 @@
 module load conda
 conda activate waveformer
 
-cd /blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wf_3211/mshead_3d
+cd /blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wf_4321/mshead_3d
 
 # Execute the Python script
 srun python main_train.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --fold $FOLD --dataset $DATASET
