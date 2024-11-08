6e3b56b1c7f1f8859da4cdbcd55c14cfa6bbad57
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-11-07 19:46:51 -0500

    plotting, organ-wise and multi-scale evaluation ready for wf_3211

---
+++ test_plotting_files/bash_test_amos.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=python_wf_2211_amos_test
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_amos/wavelet-two-branch/mshead_3d/results/wf_2211_amos_test_job.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_amos/wavelet-two-branch/mshead_3d/results/wf_2211_amos_test_job.%J.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64GB
+#SBATCH --partition=hpg-ai
+#SBATCH --gpus=a100:1
+#SBATCH --time=1:00:00
+
+module load conda
+conda activate waveformer
+
+cd /blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_amos/wavelet-two-branch/mshead_3d
+
+# Execute the Python script
+srun python test_seg.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --trained_weights $TRAINED_WEIGHTS --dataset $DATASET
---
+++ test_plotting_files/bash_test_kits.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=plot_kits_wf_3211
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wf_3211/mshead_3d/results/kits_plot_3211_job.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wf_3211/mshead_3d/results/kits_plot_3211_job.%J.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64GB
+#SBATCH --partition=hpg-ai
+#SBATCH --gpus=a100:1
+#SBATCH --time=1:00:00
+
+module load conda
+conda activate waveformer
+
+cd /blue/r.forghani/mdmahfuzalhasan/ablation_studies/ablation_kits/wf_3211/mshead_3d
+
+# Execute the Python script
+srun python plotting_prediction.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --fold $FOLD --dataset $DATASET
