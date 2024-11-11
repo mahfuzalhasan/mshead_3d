0a22471c645f8b631a1d577c4bfe89a5bd66e2d7
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-11-11 01:52:57 -0500

    waveformer 2211

--- train_job_files/bash_kits.sh
+++ train_job_files/bash_kits.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=train_kits_wf_1111_v2
-#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_1111/mshead_3d/results/kits_train_1111_v2_job.%J.out
-#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_1111/mshead_3d/results/kits_train_1111_v2_job.%J.err
+#SBATCH --job-name=train_kits_wf_2211_v2
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_2211/mshead_3d/results/kits_train_2211_v2_job.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_2211/mshead_3d/results/kits_train_2211_v2_job.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
@@ -13,7 +13,7 @@
 module load conda
 conda activate waveformer
 
-cd /blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_1111/mshead_3d
+cd /blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_2211/mshead_3d
 
 # Execute the Python script
 srun python main_train.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --fold $FOLD --dataset $DATASET
