0604f2c868c3f340b60329ec9ab54f99eff9504e
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-11-13 21:21:12 -0500

    debugging data loading

--- playground.py
+++ playground.py
@@ -13,7 +13,6 @@ for pat_id in os.listdir(dataset):
     data_file = [f for f in os.listdir(os.path.join(dataset, pat_id)) if ('.nrrd' in f) and ('Segmentation' not in f) and ('Image' not in f)][0]
     # print(f'data file:{data_file}')
     data_file_path = os.path.join(dataset, pat_id, data_file)
-    # label_file = seg_file = [j for j in os.listdir(os.path.join(dataset, pat_id)) if ('Segmentation_modified.seg' in j) or ('Image.nrrd' in j)][0]
     label_file_path = os.path.join(dataset, pat_id, 'Segmentation_modified.nrrd')
     train_img.append(data_file_path)
     train_label.append(label_file_path)
