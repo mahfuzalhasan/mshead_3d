1c8d7abbe56fae10c574ea0c553b9020ef5f7da9
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-11-13 19:08:43 -0500

    debugging

--- playground.py
+++ playground.py
@@ -14,9 +14,13 @@ for pat_id in os.listdir(dataset):
     # print(f'data file:{data_file}')
     data_file_path = os.path.join(dataset, pat_id, data_file)
     # label_file = seg_file = [j for j in os.listdir(os.path.join(dataset, pat_id)) if ('Segmentation_modified.seg' in j) or ('Image.nrrd' in j)][0]
-    label_file = os.path.join(dataset, pat_id, 'Segmentation_modified.nrrd')
-    train_img.append(data_file)
-    train_label.append(label_file)
+    label_file_path = os.path.join(dataset, pat_id, 'Segmentation_modified.nrrd')
+    train_img.append(data_file_path)
+    train_label.append(label_file_path)
+
+    print(f'path:{data_file_path} label:{label_file_path}')
 
 train_img = sorted(glob.glob(train_img))
 train_label = sorted(glob.glob(train_label))
+
+print(len(train_img), len(train_label))
