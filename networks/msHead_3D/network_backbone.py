6921cabf9ddf6564e0b53cb080a014dc011003d8
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-12-12 20:31:34 -0500

    debugging

--- networks/msHead_3D/network_backbone.py
+++ networks/msHead_3D/network_backbone.py
@@ -282,6 +282,6 @@ if __name__=="__main__":
     # # Assuming 'model' is your PyTorch model
     total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print(f"Total trainable parameters: {total_params}")
-    macs, params = get_model_complexity_info(model, (1, 14, 224, 224), as_strings=True, print_per_layer_stat=True, verbose=True)
-    print('{:<30}  {:<8}'.format('Computational complexity ptflops: ', macs))
-    print('{:<30}  {:<8}'.format('Number of parameters from ptflops: ', params))
+    # macs, params = get_model_complexity_info(model, (1, 14, 224, 224), as_strings=True, print_per_layer_stat=True, verbose=True)
+    # print('{:<30}  {:<8}'.format('Computational complexity ptflops: ', macs))
+    # print('{:<30}  {:<8}'.format('Number of parameters from ptflops: ', params))
