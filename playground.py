dba1901b86deb9c51871c3457453fece30fce4c3
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-12-16 11:14:16 -0500

    checking coeffs in wt

--- playground.py
+++ playground.py
@@ -63,6 +63,7 @@ mode = 'reflect'
 B, C, D, H, W = 2, 1, 56, 56, 56
 x= torch.randn(B, C, D, H, W)
 coeffs = ptwt.wavedec3(x, wavelet=wavelet, level=level, mode=mode)
+print(type(coeffs))
 y1 = coeffs[0]
 print(y1.shape)
 for coeff in coeffs[1:]:
