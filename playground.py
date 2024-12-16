ebd342776979ef25ee6b538b513f8ee4fd0b88bc
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-12-16 15:13:39 -0500

    debugging wt reconstruction

--- playground.py
+++ playground.py
@@ -57,33 +57,25 @@ from networks.UXNet_3D.network_backbone import UXNET
 # from monai.networks.nets import UNETR, SwinUNETR
 import ptwt
 from pytorch_wavelets import DWT3DForward, DWT3DInverse
+# import torch_dwt as dwt
 
 wavelet = 'db1'
-J = 3
+level = 3
 mode = 'reflect'
 B, C, D, H, W = 2, 1, 56, 56, 56
 x= torch.randn(B, C, D, H, W)
-# coeffs = ptwt.wavedec3(x, wavelet=wavelet, level=level, mode=mode)
-dwt3 = DWT3DForward(J=J, wave=wavelet)
-idwt3 = DWT3DInverse(wave=wavelet)
+coeffs = ptwt.wavedec3(x, wavelet=wavelet, level=level, mode=mode)
 
-yL, yh = dwt3(x)
-print(yL.shape, type(yh))
-
-yr = idwt3((yL, yh))
-
+print(type(coeffs), len(coeffs))
+y1 = coeffs[0]
+yh = coeffs[1:]
+print(type(yh), len(yh))
+print(y1.shape)
+for coeff in yh:
+    print(coeff.keys())
+    # print(coeff.shape)
+yr = ptwt.waverec3((y1, yh), wavelet=wavelet)
 print(yr.shape)
-
-# print(type(coeffs), len(coeffs))
-# y1 = coeffs[0]
-# yh = coeffs[1:]
-# print(type(yh), len(yh))
-# print(y1.shape)
-# for coeff in yh:
-#     print(coeff.keys())
-#     # print(coeff.shape)
-# yr = ptwt.waverec3((y1, yh), wavelet=wavelet)
-# print(yr.shape)
 
 
 
