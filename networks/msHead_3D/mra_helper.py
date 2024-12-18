2e993929985d195b5a326e5044ead35042994bbe
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-12-18 01:57:06 -0500

    debugging

--- networks/msHead_3D/mra_helper.py
+++ networks/msHead_3D/mra_helper.py
@@ -324,7 +324,7 @@ class Block(nn.Module):
     def forward(self, x):
         D,H,W = self.img_size
         B,_,_,_,C = x.shape
-
+        print(f'img:{self.img_size} inp_shape:{x.shape}')
         assert D == x.shape[1]
         assert H == x.shape[2]
         assert W == x.shape[3]
@@ -358,7 +358,7 @@ class Block(nn.Module):
         x = x.permute(0, 2, 3, 4, 1).contiguous()                    # B, D, H, W, C
         x = shortcut + self.drop_path(x)
         x = x + self.drop_path(self.mlp(self.norm2(x)))              # B, D, H, W, C
-        # print(f'final output:{x.shape}')
+        print(f'final output:{x.shape}')
         if self.level > 0:
             return x, x_h
         return x
