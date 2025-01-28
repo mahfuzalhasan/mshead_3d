0d2c89b81780c66d992b92fa3c52d192f5c56d2b
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2025-01-28 00:23:51 -0500

    print removed

--- main_train.py
+++ main_train.py
@@ -240,7 +240,6 @@ def train(global_step, train_loader, dice_val_best, global_step_best):
     for step, batch in enumerate(train_loader):     
         step += 1
         x, y = (batch["image"].to(device), batch["label"].to(device))       # x->B,C,H,W,D = 2,1,96,96,96. y same
-        print(f'########### type image:{x.dtype} label:{y.dtype} ###################')
         # with torch.no_grad():
         #     g_feat, dense_feat = model_feat(x)
         logit_map = model(x)
