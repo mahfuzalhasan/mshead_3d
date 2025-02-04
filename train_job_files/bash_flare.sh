7a651c2489028b3ad97eee195860314ead5269a8
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2025-02-04 00:31:44 -0500

    bash file names changed

--- train_job_files/bash_flare.sh
+++ train_job_files/bash_flare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=train_flare_wf_idwt_dec_hf_agg
-#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_1111/mshead_3d/results/train_flare_wf_idwt_dec_hf_agg.%J.out
-#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_1111/mshead_3d/results/train_flare_wf_idwt_dec_hf_agg.%J.err
+#SBATCH --job-name=train_flare_idwt_dec_hf_fused
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_1111/mshead_3d/results/train_flare_idwt_dec_hf_fused.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_1111/mshead_3d/results/train_flare_idwt_dec_hf_fused.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
