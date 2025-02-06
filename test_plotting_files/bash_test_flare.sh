3507491ec4ee07a25a94418a6dcdddf333c90f28
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2025-02-06 17:03:25 -0500

    plotting prediction for flare ablation

--- test_plotting_files/bash_test_flare.sh
+++ test_plotting_files/bash_test_flare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=plot_flare_wf_1111_idwt_dec_v2
-#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_1111/mshead_3d/results/flare_1111_plot_idwt_dec_v2.%J.out
-#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_1111/mshead_3d/results/flare_1111_plot_idwt_dec_v2.%J.err
+#SBATCH --job-name=plot_flare_ablation_pooling
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/ablation_waveformer/mshead_3d/results/plot_flare_ablation_pooling.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/ablation_waveformer/mshead_3d/results/plot_flare_ablation_pooling.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
@@ -13,7 +13,7 @@
 module load conda
 conda activate waveformer
 
-cd /blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_1111/mshead_3d
+cd /blue/r.forghani/mdmahfuzalhasan/scripts/ablation_waveformer/mshead_3d
 
 # Execute the Python script
 srun python plotting_prediction.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --fold $FOLD --dataset $DATASET --network $NETWORK
