8e4e5f6bd3c2f5cbd268b52162e40f728b5acb17
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2025-02-08 03:11:04 -0500

    job names changed

--- train_job_files/bash_flare.sh
+++ train_job_files/bash_flare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=train_flare_wf_1111_idwt_dec_v2
-#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_1111/mshead_3d/results/flare_1111_idwt_dec_v2.%J.out
-#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_1111/mshead_3d/results/flare_1111_idwt_dec_v2.%J.err
+#SBATCH --job-name=train_flare_dwt_max_pool
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Pool/mshead_3d/results/train_flare_dwt_max_pool.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Pool/mshead_3d/results/train_flare_dwt_max_pool.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
@@ -13,7 +13,7 @@
 module load conda
 conda activate waveformer
 
-cd /blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_1111/mshead_3d
+cd /blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Pool/mshead_3d
 
 # Execute the Python script
 srun python main_train.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --fold $FOLD --dataset $DATASET
