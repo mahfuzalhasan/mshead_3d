b3b835dae3bbf6d49ba44c2585c53e550eb04c7c
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2025-02-18 23:52:34 -0500

    bash file names changed

--- test_job_files/bash_test_kits.sh
+++ test_job_files/bash_test_kits.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=test_kits_wf_1111
-#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/project_analysis_kits19/mshead_3d/results/kits_test_1111_job.%J.out
-#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/project_analysis_kits19/mshead_3d/results/kits_test_1111_job.%J.err
+#SBATCH --job-name=test_kits23
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Kits/mshead_3d/results/test_kits23.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Kits/mshead_3d/results/test_kits23.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=64GB
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=128GB
 #SBATCH --partition=hpg-ai
 #SBATCH --gpus=a100:1
 #SBATCH --time=1:00:00
@@ -13,7 +13,7 @@
 module load conda
 conda activate waveformer
 
-cd /blue/r.forghani/mdmahfuzalhasan/project_analysis_kits19/mshead_3d
+cd /blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Kits/mshead_3d
 
 # Execute the Python script
 srun python test_seg.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --fold $FOLD --dataset $DATASET
--- test_plotting_files/bash_test_kits.sh
+++ test_plotting_files/bash_test_kits.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=plot_kits_wf_1111_idwt_new_dec
-#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_1111/mshead_3d/results/kits_1111_plot_idwt_new_dec.%J.out
-#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_1111/mshead_3d/results/kits_1111_plot_idwt_new_dec.%J.err
+#SBATCH --job-name=plot_test_kits23
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Kits/mshead_3d/results/plot_test_kits23.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Kits/mshead_3d/results/plot_test_kits23.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=128GB
 #SBATCH --partition=hpg-ai
 #SBATCH --gpus=a100:1
-#SBATCH --time=24:00:00
+#SBATCH --time=1:00:00
 
 module load conda
 conda activate waveformer
 
-cd /blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_1111/mshead_3d
+cd /blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Kits/mshead_3d
 
 # Execute the Python script
 srun python plotting_prediction.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --fold $FOLD --dataset $DATASET --network $NETWORK
