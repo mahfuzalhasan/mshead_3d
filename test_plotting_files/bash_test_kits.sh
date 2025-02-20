549be1ab23b900dc2fd136b86939bfe0daaa2281
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2025-02-20 08:33:25 -0500

    file names changed for kits training with Persistent Dataset

--- test_plotting_files/bash_test_kits.sh
+++ test_plotting_files/bash_test_kits.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=plot_kits_wf_1111_idwt_new_dec
-#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_1111/mshead_3d/results/kits_1111_plot_idwt_new_dec.%J.out
-#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_1111/mshead_3d/results/kits_1111_plot_idwt_new_dec.%J.err
+#SBATCH --job-name=plot_kits_hf_simple_ref_residual_up
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Improved_Up/mshead_3d/results/plot_kits_hf_simple_ref_residual_up.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Improved_Up/mshead_3d/results/plot_kits_hf_simple_ref_residual_up.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=128GB
 #SBATCH --partition=hpg-ai
 #SBATCH --gpus=a100:1
-#SBATCH --time=24:00:00
+#SBATCH --time=1:00:00
 
 module load conda
 conda activate waveformer
 
-cd /blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_1111/mshead_3d
+cd /blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Improved_Up/mshead_3d
 
 # Execute the Python script
 srun python plotting_prediction.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --fold $FOLD --dataset $DATASET --network $NETWORK
