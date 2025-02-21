70a0e31e3c7783c8953cc26a589ef253d0467339
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2025-02-21 02:09:57 -0500

    kits bash file updated

--- train_job_files/bash_kits.sh
+++ train_job_files/bash_kits.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=train_kits_hf_simple_ref_residual_up
-#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Improved_Up/mshead_3d/results/train_kits_hf_simple_ref_residual_up.%J.out
-#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Improved_Up/mshead_3d/results/train_kits_hf_simple_ref_residual_up.%J.err
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Improved_Up_kits/mshead_3d/results/train_kits_hf_simple_ref_residual_up.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Improved_Up_kits/mshead_3d/results/train_kits_hf_simple_ref_residual_up.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
@@ -13,7 +13,7 @@
 module load conda
 conda activate waveformer
 
-cd /blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Improved_Up/mshead_3d
+cd /blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Improved_Up_kits/mshead_3d
 
 # Execute the Python script
 srun python main_train.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --fold $FOLD --dataset $DATASET --network $NETWORK --resume $RESUME
