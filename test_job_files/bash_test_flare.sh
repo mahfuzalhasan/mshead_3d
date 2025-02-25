f413eb3109d14bfaf4bbd1910b799ad75838833d
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2025-02-25 14:44:26 -0500

    file path fixed in test file

--- test_job_files/bash_test_flare.sh
+++ test_job_files/bash_test_flare.sh
@@ -13,7 +13,7 @@
 module load conda
 conda activate waveformer
 
-cd /blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Multilevel
+cd /blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Multilevel/mshead_3d
 
 # Execute the Python script
 srun python test_seg.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --fold $FOLD --dataset $DATASET --network $NETWORK
