8cbab54170c2069110de79978e740609b4462389
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2025-02-26 17:09:00 -0500

    paths changed in bash files

--- test_job_files/bash_test_flare.sh
+++ test_job_files/bash_test_flare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=test_flare_multilevel_hf_simple_ref_residual_up_hf_agg
-#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Improved_Up/mshead_3d/results/test_flare_multilevel_hf_simple_ref_residual_up_hf_agg.%J.out
-#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Improved_Up/mshead_3d/results/test_flare_multilevel_hf_simple_ref_residual_up_hf_agg.%J.err
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_HF_AGG_MultiLevel/mshead_3d/results/test_flare_multilevel_hf_simple_ref_residual_up_hf_agg.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_HF_AGG_MultiLevel/mshead_3d/results/test_flare_multilevel_hf_simple_ref_residual_up_hf_agg.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
@@ -13,7 +13,7 @@
 module load conda
 conda activate waveformer
 
-cd /blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Improved_Up/mshead_3d
+cd /blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_HF_AGG_MultiLevel/mshead_3d
 
 # Execute the Python script
 srun python test_seg.py --cache_rate 1.0 --num_workers $SLURM_CPUS_PER_TASK --fold $FOLD --dataset $DATASET --network $NETWORK
--- test_organ_wise/bash_test_flare.sh
+++ test_organ_wise/bash_test_flare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=organ_flare_multilevel_hf_simple_ref_residual_up_hf_agg
-#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Improved_Up/mshead_3d/results/organ_flare_multilevel_hf_simple_ref_residual_up_hf_agg.%J.out
-#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Improved_Up/mshead_3d/results/organ_flare_multilevel_hf_simple_ref_residual_up_hf_agg.%J.err
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_HF_AGG_MultiLevel/mshead_3d/results/organ_flare_multilevel_hf_simple_ref_residual_up_hf_agg.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_HF_AGG_MultiLevel/mshead_3d/results/organ_flare_multilevel_hf_simple_ref_residual_up_hf_agg.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
@@ -13,7 +13,7 @@
 module load conda
 conda activate waveformer
 
-cd /blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_Improved_Up/mshead_3d
+cd /blue/r.forghani/mdmahfuzalhasan/scripts/WaveFormer_HF_AGG_MultiLevel/mshead_3d
 
 # Execute the Python script
 srun python multi_organ_score_flare.py --fold $FOLD --dataset $DATASET --network $NETWORK
